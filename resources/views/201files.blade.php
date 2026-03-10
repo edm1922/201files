31e5c540b5b58675bdcfaeebab823603436f1418
Ebxxx <itsebs758@gmail.com>
2026-03-10 11:14:17 +0800

    add folder code to the employee table and update the reference file

--- resources/views/201files.blade.php
+++ resources/views/201files.blade.php
@@ -101,17 +101,17 @@
                     </div>
 
                     {{-- Row 2: Secondary Information --}}
-                    <div class="col-md-4">
-                        <label class="form-label">Barcode</label>
-                        <input type="text" class="form-control field-input" placeholder="Barcode">
+                    <div class="col-md-3">
+                        <label class="form-label" for="barcodeInput">Barcode</label>
+                        <input type="text" id="barcodeInput" class="form-control field-input" placeholder="Barcode">
                     </div>
-                    <div class="col-md-4">
-                        <label class="form-label">System ID</label>
-                        <input type="text" class="form-control field-input" placeholder="System ID">
+                    <div class="col-md-3">
+                        <label class="form-label" for="systemIdInput">System ID</label>
+                        <input type="text" id="systemIdInput" class="form-control field-input" placeholder="System ID">
                     </div>
-                    <div class="col-md-4">
-                        <label class="form-label">Code</label>
-                        <input type="text" class="form-control field-input" placeholder="Code">
+                    <div class="col-md-3">
+                        <label class="form-label" for="folderCodeInput">Folder Code</label>
+                        <input type="text" id="folderCodeInput" class="form-control field-input" placeholder="Folder Code">
                     </div>
 
                     {{-- Row 3: Status and Location --}}
